6d50261da836c43e918cf34aad978ebde5b5240e
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2023-01-07 05:18:59 -0500

    typo in application.properties

---
+++ LearningSpring/lil-spring/src/main/java/com/javierlabs/lil/lilspring/data/Room.java
@@ -0,0 +1,66 @@
+package com.javierlabs.lil.lilspring.data;
+
+import javax.persistence.*;
+
+@Entity //entity annotation
+@Table(name="ROOM ") //annotate that this is a table
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    //generate columns
+    @Column(name = "ROOM_ID")
+    private long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "ROOM_NUMBER")
+    private String roomNumber;
+
+    @Column(name = "BED_INFO")
+    private String bedInfo;
+
+    //getters and setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getBedInfo() {
+        return bedInfo;
+    }
+
+    public void setBedInfo(String bedInfo) {
+        this.bedInfo = bedInfo;
+    }
+
+    //override toString() method
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", roomNumber='" + roomNumber + '\'' +
+                ", bedInfo='" + bedInfo + '\'' +
+                '}';
+    }
+}
