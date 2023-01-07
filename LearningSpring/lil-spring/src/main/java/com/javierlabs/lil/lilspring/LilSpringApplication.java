1c8c2d2d384071f3f1019bd1c562f19bd64613c9
Alex Ruperto <70246029+monstro-montra@users.noreply.github.com>
2023-01-07 04:27:14 -0500

    Add files via upload

---
+++ LearningSpring/lil-spring/src/main/java/com/javierlabs/lil/lilspring/LilSpringApplication.java
@@ -0,0 +1,14 @@
+package com.javierlabs.lil.lilspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LilSpringApplication.class, args);
+	}
+
+}
---
+++ LearningSpring/lil-spring/src/test/java/com/javierlabs/lil/lilspring/LilSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javierlabs.lil.lilspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
